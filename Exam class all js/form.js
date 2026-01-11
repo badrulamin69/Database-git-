6d2d62d357766d8dfd339754b96feb9c3243bed9
badrulamin69 <badrulamin.cu.bd@gmail.com>
2026-01-11 19:03:00 +0600

    add

--- Exam class all js/form.js
+++ Exam class all js/form.js
@@ -3,22 +3,20 @@ function form(event){
     let name = document.getElementById("name").value;
     let contact = document.getElementById("contact").value;
     let remarks = document.getElementById("remarks").value;
-    let email = document.getElementById("email").value;
     let location = document.getElementById("location").value;
 
-    let sex = document.querySelector("input[name='sex']: checked").value;
-    let course = document.querySelectorAll("input [name='course'] : checked");
+    let sex = document.querySelector("input[name='sex']:checked").value;
+    let course = document.querySelectorAll("input [name='course']:checked");
 
     let coureslist = [] ;
     for(let i=0; i<course.length; i++){
         coureslist.push(course[i].value);
     }
 
-    let output =
+    let output=
     " name " + name + "\n" +
     " contact " + contact + "\n" +
     " remarks " + remarks + "\n" +
-    " email " + email + "\n" +
     " location " + location + "\n" +
     " sex " + sex + "\n" +
     " course " + coureslist + "\n" ;
---
+++ HR db 11-01-26.sql
@@ -0,0 +1,34 @@
+--practice 1
+
+select last_name, department_id
+FROM employees
+where department_id IN
+    (select department_id
+    from employees
+    where last_name like '%u%'
+    )
+    
+    
+--  practice 2  
+    
+select last_name, department_id, job_id
+from employees
+where department_id in (
+    select department_id
+    from departments
+    where location_id=1700
+);
+
+--practice 3
+--W1-V2 chapter 9
+
+creat table students(
+    stu_id number(7) NOT NULL,
+    stu_name varchar(30) not null,
+    stu_email varchar(50) not null,
+    stu_dob date,
+    cell_no varchar2(11),
+    blood_group varchar(3),
+    
+    ADD CONSTRAINT hr_students_stu_id_pk PRIMARY KEY (stu_id)
+);
