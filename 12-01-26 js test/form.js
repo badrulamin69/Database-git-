8165dab6d1ae5c4859af2c924651d56069ce1d3d
badrulamin69 <badrulamin.cu.bd@gmail.com>
2026-01-12 18:10:42 +0600

    add

---
+++ 12-01-26 js test/form.js
@@ -0,0 +1,12 @@
+function form(event){
+    event.preventDefault()
+
+    let name = document.getElementById("name").value;
+    let contact = document.getElementById("contact").value;
+    let remarks = document.getElementById("remarks").value;
+    let location = document.getElementById("location").value;
+
+    let sex = document.getElementById("name").value;
+
+
+}
